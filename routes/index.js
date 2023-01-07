e35ed6aa3a45a0ec051eb137694da88480c9c98c
Kyle Coulon <kylejcoulon@gmail.com>
2023-01-07 11:04:57 -0700

    set up routes with views

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 const routes = require('express').Router();
+const path = require('path');
+const views_path = path.join(__dirname, '../');
 
-routes.get('/', (req, res) => {
-  res.send('Kyle Coulon');
+routes.get('/', function(req, res) {
+  res.sendFile(path.join(views_path, 'views/index.html'));
 });
 
-routes.get('/test', (req, res) => {
-  res.send('test');
+routes.get('/test', function(req, res) {
+  res.sendFile(path.join(views_path, 'views/test.html'));
+  // res.send('test .send() after sending file');
 });
+
+
 
 module.exports = routes;
